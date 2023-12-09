4e63b7a7c6b2ba195a0110b85ab6ab09a99640f0
bombh <fabrice@bxxl.be>
2023-12-09 23:17:59 +0100

    Install cookie and ios pods

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { WebView } from 'react-native-webview';
 import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+import CookieManager from '@react-native-cookies/cookies';
 
 export default function App() {
   return (
