66753595fb93f85e048a9fb4f518de7bde2ed630
bombh <fabrice@bxxl.be>
2023-12-09 19:18:39 +0100

    Start expo project

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{
+         uri: 'https://devfr.rendez-vous.be/app',
+         // headers: {
+         //    'HTTP_X_REQUESTED_WITH': 'be.rendezvous',
+         //  },
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   marginTop: Constants.statusBarHeight,
+   marginBottom: 10,
   },
 });
